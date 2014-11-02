61a1228df01ec8db9673830b1b258c324ac14c00
teragrid <onehao333@gmail.com>
2014-11-02 12:53:10 +0800

    update reference

--- pyml/mydebug.py
+++ pyml/mydebug.py
@@ -1,3 +1,17 @@
 from numpy import *;
+import kNN
 
+group,labels = kNN.createDatSet()
+
+print(group)
+
+print(labels)
+
+print(kNN.classify0([0][0], group, labels, 3))
+
+
+datingDataMat,datingLabels = kNN.file2matrix('datingTestSet.txt')
+
+    
 print(random.rand(4,4));
+
