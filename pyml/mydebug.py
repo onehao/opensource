1ce5864b2066ba1c8e095a530fbb3e4372279cce
teragrid <onehao333@gmail.com>
2014-11-02 19:57:43 +0800

    ch2, milestone1

--- pyml/mydebug.py
+++ pyml/mydebug.py
@@ -30,7 +30,16 @@ print(random.rand(4,4));
 ##matplotlib start
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
-plt.show()
+# ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
+# plt.show()
+
+
+ax.scatter(datingDataMat[:,1], datingDataMat[:,2],15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.show();
+
+print('-------datingClassTest-------------')
+kNN.datingClassTest();
+print('-------classifyPerson-------------')
+kNN.classifyPerson()
 
 
