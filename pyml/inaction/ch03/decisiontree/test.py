d356e9629aa43549c43e83b5a773f5c7c0233141
teragrid <onehao333@gmail.com>
2014-11-09 19:00:28 +0800

    resolved display Chinese for matplotlib.

--- pyml/inaction/ch03/decisiontree/test.py
+++ pyml/inaction/ch03/decisiontree/test.py
@@ -1,16 +1,25 @@
-# -*- coding: UTF-8 -*-  
+#coding=utf-8  
 '''
 @author: Michael Wan
 @since: 2014-11-08
 '''
 import trees as tr
 import treePlotter as tp
+import sys
+reload(sys)
+#http://www.pythoner.com/200.html
+#运行时没问题， 但是编译有有错误提示， 可以忽略。  
+sys.setdefaultencoding('utf8')
+print(sys.getdefaultencoding())
+
+
 
 
 #
 fr = open('lensesCN.txt')
 lenses = [unicode(inst, 'utf-8').strip().strip().split('\t') for inst in fr.readlines()]
-lensesLabels = ["年龄组" , "规定", "闪光", "泪液扫除率"]
+#lensesLabels = ["年龄组" , "规定", "闪光", "泪液扫除率"]
+lensesLabels = ['age' , 'prescript', 'astigmatic', 'tearRate']
 lensesTree = tr.createTree(lenses,lensesLabels)
 print(lensesTree)
 tp.createPlot(lensesTree)
