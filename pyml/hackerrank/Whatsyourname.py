b0ce00310329ce4f8dff82c7d3024e15132d9b7c
onehao <onehao333@gmail.com>
2014-12-29 20:08:45 +0800

    hackrank for python.

---
+++ pyml/hackerrank/Whatsyourname.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+''' hackerrank'''
+''' python version 2.7 '''
+'''
+@author: Michael Wan
+@since: 2014-12-29
+'''
+
+import sys
+
+
+firstname = str(sys.stdin.readline()).strip()
+lastname = str(sys.stdin.readline()).strip()
+
+print('Hello {0} {1}! You just delved into python.'.format(firstname, lastname))
+    
+
