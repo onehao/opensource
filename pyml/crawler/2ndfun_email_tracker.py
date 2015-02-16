8bd113f9c05be050ba52d6e714ce6ef7af482896
onehao <onehao333@gmail.com>
2015-02-16 17:58:53 +0800

    2ndfun update for MI1

--- pyml/crawler/2ndfun_email_tracker.py
+++ pyml/crawler/2ndfun_email_tracker.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 '''
-程序：LBS WEB API吧爬虫
+程序：focus on Mi1 on ganji & 58
 版本：0.1
 @author: Michael Wan
-日期：2014-11-15
+日期：2015.02.16
 语言：Python 2.7
 操作：输入带分页的地址，去掉最后面的数字，设置一下起始页数和终点页数。
 功能：下载对应页码内的所有页面并存储为html文件。
@@ -22,26 +22,35 @@ from crawler.myemail import send_mail
 #运行时没问题， 但是编译有有错误提示， 可以忽略。  
 #sys.setdefaultencoding('utf8')
 crawler.logger.info(sys.getdefaultencoding())
-mailto_list=["wanhao01@baidu.com", 'onehaojacket@gmail.com','174823192@qq.com'] 
+mailto_list=['onehaojacket@gmail.com', '174823192@qq.com','onehaobaidu@163.com'] 
 
 
-def get4PageForum(url,title):
+def get4PageForum(url,title,page):
     #url = 'http://bbs.lbsyun.baidu.com/forum.php?mod=forumdisplay&fid=7&page='
     result = ""
     result2 = []
     crawler.logger.info("test")
-    for i in range(1, 4):
+    
+    
+    
+    #section pattern for genji
+    pattern = re.compile('<dl[\s]+class=\"list-bigpic[\s]+clearfix\" >.*?</dl>',re.DOTALL)
+    patternTitle = re.compile('<a href.*?</a>',re.DOTALL)
+    patternComment = re.compile("<p.*?</p>")
+    
+    for i in range(1, page):
         sName = string.zfill(i,5) #+ '.html'#自动填充成六位的文件名
         crawler.logger.info('正在下载第' + str(i) + '个网页，并将其存储为' + sName + '......')
         
         #f = open(sName,'w+')
         m = urllib2.urlopen(url + str(i)).read()
-        #print(m)
-        regex = '<a[\s]+href=\"forum.php\?mod=viewthread.*\"[\s]+onclick=\"atarget\(this\)\"[\s]+class=\"s[\s]xst\"[\s]>.*</a>'
+        
+        a = "<dl class=\"list-bigpic clearfix\" >[]<dt>"
         # 将正则表达式编译成Pattern对象
-        pattern = re.compile(r'<!--[\s]+主题名称[\s]+-->[\s]+<a[\s]+href=\"forum.php\?mod=viewthread.*\"[\s]+onclick=\"atarget\(this\)\"[\s]+class=\"s[\s]xst\"[\s]>.*</a>')
-        pattern2 = re.compile('<a[\s]+href=\"forum.php\?mod=viewthread.*\"[\s]+onclick=\"atarget\(this\)\"[\s]+class=\"s[\s]xst\"[\s]>.*</a>')
+
         match = pattern.findall(m)
+        
+        print(len(match))
 #         match = re.findall(regex,m)
 #         print(match)
 #         match = re.compile(regex).search(m)
@@ -50,17 +59,20 @@ def get4PageForum(url,title):
         #match = pattern2.findall(match)
         crawler.logger.info(match)
         result2 +=match
-        for i in range(0,len(match)):
-            test = pattern2.findall(match[i])
-            match[i] = pattern2.findall(match[i])[0]
-            result += match[i].replace("forum.php", "bbs.lbsyun.baidu.com/forum.php") + "<br />"
-    if send_mail(mailto_list, 'LBS WEB API forum _ ' + title+"_"+ time.strftime("%Y-%m-%d", time.localtime()), result):
+        for i in range(0,len(match)-1):
+            forumtitle = patternTitle.findall(match[i])
+            try:
+                forumtitle[0] = forumtitle[0].replace(" class=\"ft-tit\"","")
+                comment = patternComment.findall(match[i])
+                
+                result += forumtitle[0] + " (" + comment[0] + " --- " + comment[1] +  ")" + "<br />"
+            except:
+                continue
+    if send_mail(mailto_list, '小米1 _ ' + title+"_"+ time.strftime("%Y-%m-%d", time.localtime()), result):
         crawler.logger.info("发送成功".decode('utf8'))
     else:  
         crawler.logger.error('发送失败'.decode('utf8'))
     #send_mail(mailto_list, 'LBS WEB API forum _2' + time.strftime("%Y-%m-%d", time.localtime()), result2)
 
-get4PageForum('http://bbs.lbsyun.baidu.com/forum.php?mod=forumdisplay&fid=7&page=',"WEB_API")
-get4PageForum('http://bbs.lbsyun.baidu.com/forum.php?mod=forumdisplay&fid=6&page=',"LBS云")
-get4PageForum('http://bbs.lbsyun.baidu.com/forum.php?mod=viewthread&tid=81406&extra=page=',"鹰眼")
+get4PageForum('http://bj.ganji.com/mi/changping/p2/?p=',"ganji",2)
 #raw_input()
