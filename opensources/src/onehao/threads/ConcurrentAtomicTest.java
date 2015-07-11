3a15abcc4aea1f1a6e7fc4a09d5d1c71cefe856e
teragrid <onehao333@gmail.com>
2015-07-11 22:44:34 +0800

    : 8-java5原子性操作类的应用
    Task-Url:

---
+++ opensources/src/onehao/threads/ConcurrentAtomicTest.java
@@ -0,0 +1,18 @@
+package onehao.threads;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/***
+ * investigate the concurrent.atomic package.
+ * @author Michael Wan
+ *
+ */
+public class ConcurrentAtomicTest {
+	public static void main(String[] args) {
+		AtomicInteger i = new AtomicInteger(0);
+		for(;i.get()<50;i.incrementAndGet()){
+			System.out.println(i);
+		}
+		
+	}
+}
