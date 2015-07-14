357fe6fa458c1d245e9064969cb41774eafce113
teragrid <onehao333@gmail.com>
2015-07-14 21:41:46 +0800

    : 19-interview
    Task-Url:

---
+++ opensources/src/queue/Test.java
@@ -0,0 +1,60 @@
+package queue;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.SynchronousQueue;
+
+public class Test {
+
+	public static void main(String[] args) {
+		final Semaphore semaphore = new Semaphore(1);
+		final SynchronousQueue<String> queue = new SynchronousQueue<String>();
+		ExecutorService executors = Executors.newFixedThreadPool(10);
+		for (int i = 0; i < 10; i++) {
+			executors.submit(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						semaphore.acquire();
+						String input = queue.take();
+						String output = TestDo.doSome(input);
+						System.out.println(Thread.currentThread().getName()
+								+ ":" + output);
+
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} finally {
+						semaphore.release();
+					}
+				}
+			});
+		}
+
+		System.out.println("begin:" + (System.currentTimeMillis() / 1000));
+		for (int i = 0; i < 10; i++) { // 这行不能改动
+			String input = i + ""; // 这行不能改动
+			try {
+				queue.put(input);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+// 不能改动此TestDo类
+class TestDo {
+	public static String doSome(String input) {
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		String output = input + ":" + (System.currentTimeMillis() / 1000);
+		return output;
+	}
+}
