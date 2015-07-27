624c31e40a136f50dd8d6c8831d457e6d513df10
onehao <onehao333@gmail.com>
2015-07-27 16:57:31 +0800

    add red-black implementation of the kth largest element.

--- lmleetproject/src/michael/leetcode/KthLargest.java
+++ lmleetproject/src/michael/leetcode/KthLargest.java
@@ -2,6 +2,7 @@ package michael.leetcode;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.TreeSet;
 
 /**
  * Find the kth largest element in an unsorted array. Note that it is the kth
@@ -142,11 +143,52 @@ public class KthLargest {
 		}
 	}
 
+	// define a tree.
+	private TreeSet<Integer> tree = null;
+
+	/**
+	 * Using red-black tree(based on tree set) to resolve the problem.
+	 * 
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public int findKthLargest4(int[] nums, int k) {
+		tree = new TreeSet<Integer>(new Comparator<Integer>(){
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				if(o1 > o2){
+					return 1;
+				}else{
+					return -1;
+				}
+			}
+		});
+		
+		for (int num : nums) {
+			// store in a min heap with max size K.
+			if (tree.size() < k) {
+				tree.add(num);
+			} else {
+				// replace the head of the min heap, if the present element is
+				// larger than the head.
+				if (tree.first() < num) {
+					tree.pollFirst();
+					tree.add(num);
+				}
+			}
+		}
+
+		return tree.first();
+	}
+
 	public static void main(String[] args) {
 		int[] nums = new int[] { 3, 2, 1, 5, 6, 4, 7, 10, 8, 9, 22 };
 		System.out.println(new KthLargest().findKthLargest(nums, 2));
 		System.out.println(new KthLargest().findKthLargest2(nums, 2));
 		System.out.println(new KthLargest().findKthLargest3(nums, 2));
+		System.out.println(new KthLargest().findKthLargest4(nums, 2));
 
 		int[] nums2 = new int[] { 1, 2 };
 		System.out.println(new KthLargest().findKthLargest3(nums2, 1));
