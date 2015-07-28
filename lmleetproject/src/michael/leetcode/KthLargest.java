f6e5cd090830276bf3bc5530d7f438c1c4b0dd01
onehao <onehao333@gmail.com>
2015-07-28 14:43:28 +0800

    add using skip list to resolve the problem.

--- lmleetproject/src/michael/leetcode/KthLargest.java
+++ lmleetproject/src/michael/leetcode/KthLargest.java
@@ -3,6 +3,7 @@ package michael.leetcode;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * Find the kth largest element in an unsorted array. Note that it is the kth
@@ -182,6 +183,46 @@ public class KthLargest {
 
 		return tree.first();
 	}
+	
+	//define a skip list.
+	private ConcurrentSkipListSet<Integer> skipList = null;
+	
+	/**
+	 * Using skip list to resolve the problem.
+	 * 
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public int findKthLargest5(int[] nums, int k) {
+		skipList = new ConcurrentSkipListSet<Integer>(new Comparator<Integer>(){
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				if(o1 > o2){
+					return 1;
+				}else{
+					return -1;
+				}
+			}
+		});
+		
+		for (int num : nums) {
+			// store in a min heap with max size K.
+			if (skipList.size() < k) {
+				skipList.add(num);
+			} else {
+				// replace the head of the min heap, if the present element is
+				// larger than the head.
+				if (skipList.first() < num) {
+					skipList.pollFirst();
+					skipList.add(num);
+				}
+			}
+		}
+
+		return skipList.first();
+	}
 
 	public static void main(String[] args) {
 		int[] nums = new int[] { 3, 2, 1, 5, 6, 4, 7, 10, 8, 9, 22 };
@@ -189,6 +230,7 @@ public class KthLargest {
 		System.out.println(new KthLargest().findKthLargest2(nums, 2));
 		System.out.println(new KthLargest().findKthLargest3(nums, 2));
 		System.out.println(new KthLargest().findKthLargest4(nums, 2));
+		System.out.println(new KthLargest().findKthLargest5(nums, 2));
 
 		int[] nums2 = new int[] { 1, 2 };
 		System.out.println(new KthLargest().findKthLargest3(nums2, 1));
