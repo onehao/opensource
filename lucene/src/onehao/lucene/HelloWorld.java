5f22ab815388003adb7bc7c2df251cbb6912f653
hackerone111 <onehaohacker@gmail.com>
2015-07-28 22:40:52 +0800

    t

--- lucene/src/onehao/lucene/HelloWorld.java
+++ lucene/src/onehao/lucene/HelloWorld.java
@@ -1,7 +1,6 @@
 package onehao.lucene;
 
 import java.io.IOException;
-import java.util.Collection;
 
 import onehao.lucene.utils.FileToDocument;
 
@@ -10,10 +9,7 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.IOContext;
-import org.apache.lucene.store.IndexInput;
-import org.apache.lucene.store.IndexOutput;
-import org.apache.lucene.store.Lock;
+import org.apache.lucene.store.FSDirectory;
 import org.junit.Test;
 
 public class HelloWorld {
@@ -31,64 +27,8 @@ public class HelloWorld {
 		// file --> document.
 		Document doc = FileToDocument.file2Document(filePath);
 		
-		IndexWriter indexWriter = new IndexWriter(new Directory() {
-			
-			@Override
-			public void sync(Collection<String> names) throws IOException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void renameFile(String source, String dest) throws IOException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public IndexInput openInput(String name, IOContext context)
-					throws IOException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Lock makeLock(String name) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public String[] listAll() throws IOException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public long fileLength(String name) throws IOException {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public void deleteFile(String name) throws IOException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public IndexOutput createOutput(String name, IOContext context)
-					throws IOException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public void close() throws IOException {
-				// TODO Auto-generated method stub
-				
-			}
-		}, conf);
+		Directory dir = FSDirectory.ge
+		IndexWriter indexWriter = new IndexWriter(, conf);
 		indexWriter.addDocument(doc);
 		
 		indexWriter.close();
