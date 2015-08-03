f777824aa3a741aae2bef19a6f41862f34e30e28
onehao <onehao333@gmail.com>
2015-08-03 17:47:30 +0800

    check in not finished.

---
+++ lmleetproject/src/michael/leetcode/linkedlist/RemoveDuplicateFromSortedList2.java
@@ -0,0 +1,64 @@
+package michael.leetcode.linkedlist;
+
+/**
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ * 
+ * For example, Given 1->2->3->3->4->4->5, return 1->2->5. Given 1->1->1->2->3,
+ * return 2->3.
+ * 
+ * @author Michael Wan.
+ *
+ */
+public class RemoveDuplicateFromSortedList2 {
+	public ListNode deleteDuplicates(ListNode head) {
+		// do make sure to check boundaries.
+		if (head == null || head.next == null) {
+			return head;
+		}
+		ListNode result = new ListNode(0);
+		ListNode resultHead = result;
+		ListNode checkDup = head;
+		boolean isRemoveCurrent = false;
+		while (head.next != null) {
+			if (head.val == head.next.val) {
+				head.next = head.next.next;
+				isRemoveCurrent = true;
+				continue;
+			}
+			if(!isRemoveCurrent){
+				result.next = checkDup;
+			}else{
+				result.next = head.next;
+			}
+			result = result.next;
+			checkDup = head.next;
+			isRemoveCurrent = false;
+			head = checkDup;
+			
+		}
+
+		return resultHead.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode node1 = new ListNode(1);
+		ListNode node2 = (node1.next = new ListNode(1));
+		ListNode node3 = (node2.next = new ListNode(2));
+		ListNode node4 = (node3.next = new ListNode(3));
+		ListNode node5 = (node4.next = new ListNode(6));
+		ListNode node6 = (node5.next = new ListNode(6));
+
+		ListNode recorderList = new RemoveDuplicateFromSortedList2()
+				.deleteDuplicates(node1);
+		printNode(recorderList);
+
+	}
+
+	private static void printNode(ListNode node) {
+		while (node != null) {
+			System.out.print(String.format("%s->", node.val));
+			node = node.next;
+		}
+	}
+}
