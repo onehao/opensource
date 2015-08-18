2f9b68df7c152367f9188a00679edafe95bf8169
onehao <onehao333@gmail.com>
2015-08-18 11:18:45 +0800

    update

--- lmleetproject/src/michael/leetcode/tree/FlattenBinaryTreeToLinkedList.java
+++ lmleetproject/src/michael/leetcode/tree/FlattenBinaryTreeToLinkedList.java
@@ -5,39 +5,37 @@ import java.util.Stack;
 import michael.leetcode.basestructure.TreeNode;
 
 public class FlattenBinaryTreeToLinkedList {
-	public void flatten(TreeNode root) {
 
+	public void flatten(TreeNode root) {
 		if (null == root) {
 			return;
 		}
-		TreeNode result = root;
-		addNode(result, root);
-		
+		current = root;
+		addNode(root);
 	}
+	
+	private TreeNode current;
 
-	private void addNode(TreeNode current, TreeNode node) {
+	private void addNode(TreeNode node) {
 		//must check boundaries.
 		if(null == node){
 			return ;
 		}
 		TreeNode right = node.right;
+		TreeNode left = node.left;
 		if(node.left != null){
-			current.right = node.left;
+			current.right = left;
 			current.left = null;
 			current = current.right;
-			
-			addNode(current, node.left);
-			
+			addNode(left);
 		}
 		
 		if(right != null){
 			current.right = right;
 			current.left = null;
 			current = current.right;
-			addNode(current, right);
+			addNode(right);
 		}
-		
-		
 	}
 	
 	public void flatten2(TreeNode root) {
@@ -66,8 +64,12 @@ public class FlattenBinaryTreeToLinkedList {
 		TreeNode n1 = new TreeNode(1);
 		TreeNode n2 = new TreeNode(2);
 		TreeNode n3 = new TreeNode(3);
+		TreeNode n4 = new TreeNode(4);
+		TreeNode n5 = new TreeNode(5);
 		n1.left = n2;
 		n2.left = n3;
+		n2.right = n4;
+		n3.left = n5;
 		new FlattenBinaryTreeToLinkedList().flatten(n1);
 		
 		BSTIterator iterator = new BSTIterator(n1);
