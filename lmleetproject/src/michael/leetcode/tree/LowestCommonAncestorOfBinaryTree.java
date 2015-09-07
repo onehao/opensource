99227dec731519ad424af359a39b67aeee54b574
hackerone111 <hackerone@gmail.com>
2015-09-08 00:05:33 +0800

    test

--- lmleetproject/src/michael/leetcode/tree/LowestCommonAncestorOfBinaryTree.java
+++ lmleetproject/src/michael/leetcode/tree/LowestCommonAncestorOfBinaryTree.java
@@ -1,5 +1,7 @@
 package michael.leetcode.tree;
 
+import michael.leetcode.basestructure.TreeNode;
+
 public class LowestCommonAncestorOfBinaryTree {
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
