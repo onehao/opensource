d984278d6933a36eca678fd8a2c16ad8b6d2cd10
hackerone111 <onehaohacker@gmail.com>
2016-04-14 20:05:56 +0800

    : python unit, update diff
    Task-Url:

--- pyml/difflib/jsondiff.py
+++ pyml/difflib/jsondiff.py
@@ -23,8 +23,7 @@ class MyJsonDiff:
     
     def getDiffList(self):
         return list(self.differ.compare(self.str1, self.str2))
-    
-    
+      
 if __name__ == '__main__':
     str1 = '{"code":1"items":[],"batch_id":"HQOYL2hF-9ay3-IFq8-mqCX-mfKGXk2P87j5","message":"Successful.","version":"m-1.0-SNAPSHOT-1921","timestamp":"1460537067","result":true,"done":1,"ver":"Vw4G66VNk2dhfn9f",}'
     str2 = '{"code":1,"message":"Successful.","timestamp":"1460537067.229","version":"m-1.0-SNAPSHOT-1921","result":true,"done":1,"ver":"Vw4G66VNk2dhfn9f","items":[],"batch_id":"HQOYL2hF-9ay3-IFq8-mqCX-mfKGXk2P87j4"}'
