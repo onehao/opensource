34b7c1cd65d728aaeb96ab37f92b757998a373b4
onehao <onehao333@gmail.com>
2016-04-27 14:14:39 +0800

    : python unit
    Task-Url:

--- pyml/tools/getip.py
+++ pyml/tools/getip.py
@@ -6,7 +6,7 @@ Created on 2016年4月27日
 '''
 
 import socket
-localIP = socket.gethostbyname(socket.gethostname())#这个得到本地ip
+localIP = socket.gethostbyname(socket.gethostname())#这个得到本地ip 
 print "local ip:%s "%localIP
 ipList = socket.gethostbyname_ex(socket.gethostname())
 for i in ipList:
