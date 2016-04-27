583233000a9f4b4aafb95bf5a09eed6e2ad4cc90
onehao <onehao333@gmail.com>
2016-04-27 14:19:30 +0800

    : python: getip

--- pyml/tools/getip.py
+++ pyml/tools/getip.py
@@ -12,3 +12,24 @@ ipList = socket.gethostbyname_ex(socket.gethostname())
 for i in ipList:
     if i != localIP:
        print "external IP:%s"%i
+       
+#!/usr/bin/env python
+import re,urllib2
+class Get_public_ip:
+    def getip(self):
+        try:
+            myip = self.visit("http://www.111cn.net/")
+        except:
+            try:
+                myip = self.visit("http://www.ip138.com/ip2city.asp")
+            except:
+                myip = "So sorry!!!"
+        return myip
+    def visit(self,url):
+        opener = urllib2.urlopen(url)
+        if url == opener.geturl():
+            str = opener.read()
+        return re.search('d+.d+.d+.d+',str).group(0)
+if __name__ == "__main__":
+    getmyip = Get_public_ip()
+    print getmyip.getip()
