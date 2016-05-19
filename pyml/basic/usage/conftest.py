7478d24102dc7710e185073da1cbffa8d4ccbd8b
onehao <onehao333@gmail.com>
2016-05-19 09:35:32 +0800

    pytest plugin for customized report.
    Task-Url:

--- pyml/basic/usage/conftest.py
+++ pyml/basic/usage/conftest.py
@@ -4,13 +4,26 @@ Created on 2016年5月18日
 
 @author: michael.wh
 '''
+import sys
+
 import pytest
 
-@pytest.mark.tryfirst
-def pytest_runtest_makereport(item, call, __multicall__):
-    # execute all other hooks to obtain the report object
-    rep = __multicall__.execute()
-    if rep.when == "call":
-        extra = '{0}::{1} -- "{2}"'.format(item._location[0], item._location[2], '' if item._obj.__doc__ is None else item._obj.__doc__.strip())
-        rep.nodeid =  extra
-    return rep
+
+sys.path.append('../plugins')
+pytest_plugins = "report"
+# @pytest.mark.tryfirst
+# def pytest_runtest_makereport(item, call, __multicall__):
+#     # execute all other hooks to obtain the report object
+#     rep = __multicall__.execute()
+#     if rep.when == "call":
+#         extra = '{0}::{1} -- "{2}"'.format(item._location[0], item._location[2], '' if item._obj.__doc__ is None else item._obj.__doc__.strip())
+#         rep.nodeid = extra
+#     if rep.when == "setup":
+#         rep.nodeid = ""
+#     return rep
+# @pytest.hookimpl(tryfirst=True)
+# def pytest_collection_modifyitems(session, config, items):
+#     """ called after collection has been performed, may filter or re-order
+#     the items in-place."""
+#     print(str(items))
+#     pass
--- pyml/basic/usage/testdescription.py
+++ pyml/basic/usage/testdescription.py
@@ -7,6 +7,9 @@ Created on 2016年5月18日
 import unittest
 from unittest.runner import TextTestResult
 import pytest
+import sys
+# sys.path.append('../plugins')
+# pytest_plugins = "report"
 
 TextTestResult.getDescription = lambda _, test: test.shortDescription()
 class DemoTest(unittest.TestCase):
@@ -21,5 +24,14 @@ class DemoTest(unittest.TestCase):
     
     @pytest.mark.p1
     def test_foo2(self):
+        self.assertTrue(False)
+        pass
+    
+    @pytest.mark.p1
+    def test_foo3(self):
+        """Test foo this 
+        is michael, 
+        hah
+        """
         self.assertTrue(False)
         pass
